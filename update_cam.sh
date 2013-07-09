031ae7fd1a718f6add5dd91e392d9992da92a55d
Jorrit Poelen <jhpoelen@gmail.com>
2013-07-09 10:44:10 -0700

    push image to server

--- update_cam.sh
+++ update_cam.sh
@@ -1,1 +1,5 @@
-echo `date` hello world
+echo `date` update cam starting...
+rm /tmp/view.jpg
+curl http://sv.berkeley.edu/view/images/newview.jpg > /tmp/view.jpg
+curl -F "view=@/tmp/view.jpg" -v http://cal-sailing.appspot.com/cam
+echo `date` update cam done.
