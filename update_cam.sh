dc422e8f83dbc130fb363c1eb448720f553ea926
Jorrit Poelen <jhpoelen@gmail.com>
2013-07-10 15:57:00 -0700

    enabled cam upload with cam settings

--- update_cam.sh
+++ update_cam.sh
@@ -1,12 +1,16 @@
 function update_cam {
 	echo `date` update cam [$2] starting...
-	tmp_file = /tmp/$2.jpg
-	rm $tmp_file
-	curl http://$1/img/snapshot.cgi?[size=3][&quality=3] > $tmp_file
-	curl -F "$2=@/tmp/$2.jpg" -v http://cal-sailing.appspot.com/cam
-	rm $tmp_file
+	TMP_FILE=/tmp/$2.jpg
+	curl --globoff "http://$1/img/snapshot.cgi?[size=3][&quality=3]" > $TMP_FILE
+	if [ -f $TMP_FILE ] 
+	then
+		curl -F "$2=@$TMP_FILE" -v http://cal-sailing.appspot.com/cam
+		rm $TMP_FILE
+	else
+		echo cam image [$TMP_FILE] not available 
+	fi
 	echo `date` update cam [$2] done.
 }
 
-update_cam 192.168.1.252 restaurant_test
-update_cam 192.168.1.253 dock_test
+update_cam 192.168.1.252 restaurant
+update_cam 192.168.1.253 dock
