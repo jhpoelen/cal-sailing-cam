a86b3fd7dc2df6e3e4d9c118c12efa48324f2408
Jorrit Poelen <jhpoelen@gmail.com>
2013-07-10 14:36:22 -0700

    added actual ips of cams

--- update_cam.sh
+++ update_cam.sh
@@ -1,5 +1,12 @@
-echo `date` update cam starting...
-rm /tmp/view.jpg
-curl http://sv.berkeley.edu/view/images/newview.jpg > /tmp/view.jpg
-curl -F "view=@/tmp/view.jpg" -v http://cal-sailing.appspot.com/cam
-echo `date` update cam done.
+function update_cam() {
+	echo `date` update cam [$2] starting...
+	tmp_file = /tmp/$2.jpg
+	rm $tmp_file
+	curl http://$1/img/snapshot.cgi?[size=3][&quality=3] > $tmp_file
+	curl -F "$2=@/tmp/$2.jpg" -v http://cal-sailing.appspot.com/cam
+	rm $tmp_file
+	echo `date` update cam [$2] done.
+}
+
+update_cam 192.168.1.252 restaurant_test
+update_cam 192.168.1.253 dock_test
