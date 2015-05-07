d67367abf46e52312b46c3ebe6097398d8c747d8
Jorrit Poelen <jhpoelen@gmail.com>
2015-05-06 19:33:00 -0700

    added new hd cam for restaurant

--- update_cam.sh
+++ update_cam.sh
@@ -14,7 +14,21 @@ update_cam() {
 	echo `date` update cam [$2] done.
 }
 
+update_hd_cam() {
+	echo `date` update cam [$2] starting...
+	TMP_FILE=/tmp/$2.jpg
+	avconv "http://$1:554" > $TMP_FILE
+	if [ -f $TMP_FILE ] 
+	then
+		curl -F "$2=@$TMP_FILE" -v http://cal-sailing.appspot.com/cam
+		rm $TMP_FILE
+	else
+		echo cam image [$TMP_FILE] not available 
+	fi
+	echo `date` update cam [$2] done.
+}
+
 curl http://cal-sailing.appspot.com/ping
-update_cam 192.168.1.252 restaurant
+update_hd_cam 192.168.1.10 restaurant
 update_cam 192.168.1.253 dock
 
