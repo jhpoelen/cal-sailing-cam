10670e72b356830bb6222d6b57ae343ebf0db36d
Jorrit Poelen <jhpoelen@xs4all.nl>
2019-04-16 20:49:53 -0700

    Update update_cam.sh

--- update_cam.sh
+++ update_cam.sh
@@ -32,7 +32,10 @@ update_whiteboard_cam() {
 	echo `date` update cam [$2] starting...
 	TMP_FILE=/tmp/$2.jpg
 	#avconv -i "rtsp://$1:34567" $TMP_FILE
-	#ping $1 > $TMP_FILE
+	# kill previous ping processes
+	ps -e  | grep " ping$" | cut -d ' ' -f1 | xargs kill
+	# ping five times
+	ping -c 5 $1 > $TMP_FILE
 	if [ -f $TMP_FILE ] 
 	then
 		curl -F "$2=@$TMP_FILE" -v http://cal-sailing.appspot.com/cam
