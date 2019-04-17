0f0db6445e8b28816e306ef0262f87ca2d4cc360
jhpoelen <jhpoelen@xs4all.nl>
2019-04-17 10:33:59 -0700

    add alternate method for grabbing frame

--- update_cam.sh
+++ update_cam.sh
@@ -42,7 +42,23 @@ update_whiteboard_cam() {
 	echo `date` update cam [$2] done.
 }
 
+update_whiteboard2_cam() {
+	echo `date` update cam [$2] starting...
+	TMP_FILE=/tmp/$2.jpg
+	curl --max-time 30 http://$1/webcapture.jpg?command=snap&chanel=1 > $TMP_FILE
+	if [ -f $TMP_FILE ] 
+	then
+		curl -F "$2=@$TMP_FILE" -v http://cal-sailing.appspot.com/cam
+		rm $TMP_FILE
+	else
+		echo cam image [$TMP_FILE] not available 
+	fi
+	echo `date` update cam [$2] done.
+}
+
+
 curl http://cal-sailing.appspot.com/ping
 update_hd_cam 192.168.1.10 restaurant
 update_cam 192.168.1.253 dock
 update_whiteboard_cam 192.168.1.12 whiteboard
+update_whiteboard2_cam 192.168.1.12 whiteboard2
