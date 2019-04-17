d844339f0b7fbad5f0b92e606f931050579ec9f0
Jorrit Poelen <jhpoelen@xs4all.nl>
2019-04-16 20:33:16 -0700

    do ping first

--- update_cam.sh
+++ update_cam.sh
@@ -28,9 +28,22 @@ update_hd_cam() {
 	echo `date` update cam [$2] done.
 }
 
+update_whiteboard_cam() {
+	echo `date` update cam [$2] starting...
+	TMP_FILE=/tmp/$2.jpg
+	#avconv -i "rtsp://$1:34567" $TMP_FILE
+	ping $1 > $TMP_FILE
+	if [ -f $TMP_FILE ] 
+	then
+		curl -F "$2=@$TMP_FILE" -v http://cal-sailing.appspot.com/cam
+		rm $TMP_FILE
+	else
+		echo cam image [$TMP_FILE] not available 
+	fi
+	echo `date` update cam [$2] done.
+}
+
 curl http://cal-sailing.appspot.com/ping
 update_hd_cam 192.168.1.10 restaurant
 update_cam 192.168.1.253 dock
-update_hd_cam 192.168.1.10 whiteboard
-#update_hd_cam 192.168.1.12 whiteboard
-
+update_whiteboard_cam 192.168.1.12 whiteboard
