28019ceea904d3e9aebba3d4dfa958c7b7c890b6
jhpoelen <jhpoelen@xs4all.nl>
2020-05-13 08:20:21 -0700

    disable coin toss for now

--- update_cam.sh
+++ update_cam.sh
@@ -58,12 +58,12 @@ update_whiteboard_lowres_cam() {
 
 # reduce load on server by only processing every other 5 cycles
 COIN_TOSS=$(($RANDOM % 6))
-if [ $COIN_TOSS -eq 0 ] 
-then
+#if [ $COIN_TOSS -eq 0 ] 
+#then
   curl http://cal-sailing.appspot.com/ping?date=$(date +%s)
   update_hd_cam 192.168.1.10 restaurant_cam
   update_cam 192.168.1.253 dock_cam
   update_whiteboard_lowres_cam 192.168.1.12 whiteboard
   #update_whiteboard_hires_cam 192.168.1.12 whiteboard
-fi
+#fi
 
