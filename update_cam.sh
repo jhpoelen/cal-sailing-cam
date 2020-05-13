e4748f4fce18cb93b9e63c12bd1ea97f995114fd
jhpoelen <jhpoelen@xs4all.nl>
2020-05-13 11:18:53 -0700

    yet another attempt to get random coin toss on sh/raspian

--- update_cam.sh
+++ update_cam.sh
@@ -58,7 +58,7 @@ update_whiteboard_lowres_cam() {
 
 
 NOW_UNIX_EPOCH=$(date +%s)
-COIN_TOSS=$(shuf -i1-6 -n1)
+COIN_TOSS=$(od -An -N1 -i  /dev/urandom | head -n1 | tr -d ' ')
 PING_URL=$(echo http://cal-sailing.appspot.com/ping?date=$NOW_UNIX_EPOCH&coin_toss=$COIN_TOSS)
 curl "$PING_URL"
 update_hd_cam 192.168.1.10 restaurant
