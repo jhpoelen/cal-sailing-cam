9f670c7820b006fd7abf317fa7fa4e1f200c5d6a
jhpoelen <jhpoelen@xs4all.nl>
2020-09-05 22:14:48 -0700

    another attempt to implement streaming for avconv

--- update_cam.sh
+++ update_cam.sh
@@ -10,40 +10,12 @@ update_cam() {
 }
 
 update_hd_cam() {
-	echo `date` update cam [$2] starting...
-	TMP_FILE=/tmp/$2.jpg
-	avconv -i "rtsp://$1:554" $TMP_FILE
-	if [ -f $TMP_FILE ] 
-	then
-		curl -F "$2=@$TMP_FILE" -v $UPLOAD_URL
-		rm $TMP_FILE
-	else
-		echo cam image [$TMP_FILE] not available 
-	fi
-	echo `date` update cam [$2] done.
-}
-
-update_hd_cam2() {
         echo `date` update cam [$2] starting...
         avconv -i "rtsp://$1:554" -f image2 -frames:v 1 pipe:1 | curl -F $2=@- -v $UPLOAD_URL
         echo `date` update cam [$2] done.
 }
 
 update_whiteboard_lowres_cam() {
-	echo `date` update cam [$2] starting...
-	TMP_FILE=/tmp/$2.jpg
-	avconv -i "rtsp://$1:554/user=admin&password=&channel=1&stream=1.sdp?real_stream--rtp-caching=100" $TMP_FILE
-	if [ -f $TMP_FILE ] 
-	then
-		curl -F "$2=@$TMP_FILE" -v https://whatskraken.cal-sailing.org/cam
-		rm $TMP_FILE
-	else
-		echo cam image [$TMP_FILE] not available 
-	fi
-	echo `date` update cam [$2] done.
-}
-
-update_whiteboard_lowres_cam2() {
 	echo `date` update cam [$2] starting...
 	avconv -i "rtsp://$1:554/user=admin&password=&channel=1&stream=1.sdp?real_stream--rtp-caching=100" -f image2 -frames:v 1 pipe:1 | curl -F $2=@- -v $UPLOAD_URL
 	echo `date` update cam [$2] done.
@@ -61,8 +33,6 @@ ps -e | grep avconv | sed -E 's/^\s+//g' | cut -d ' ' -f1 | xargs -L1 kill
 curl $SERVER_URL/ping
 update_hd_cam 192.168.1.10 restaurant
 
-#update_cam 192.168.1.253 dock
-update_cam2 192.168.1.253 dock
+update_cam 192.168.1.253 dock
 
-#update_whiteboard_lowres_cam 192.168.1.12 whiteboard
-update_whiteboard_lowres_cam2 192.168.1.12 whiteboard
+update_whiteboard_lowres_cam 192.168.1.12 whiteboard
