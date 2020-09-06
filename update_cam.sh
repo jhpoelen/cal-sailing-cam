dd0616712b7730c4f5a133be3c9c3f1b1272501b
jhpoelen <jhpoelen@xs4all.nl>
2020-09-05 21:11:15 -0700

    attempt to stop zombie avconv processes

--- update_cam.sh
+++ update_cam.sh
@@ -49,8 +49,14 @@ update_whiteboard_lowres_cam2() {
 	echo `date` update cam [$2] done.
 }
 
+# upload list of running pi processes
 ps -ef | curl -F log=@- -v $UPLOAD_URL
-ps -e | grep avconv | cut -d ' ' -f1 | xargs -L1 echo kill -9 | curl -F log_kill=@- -v $UPLOAD_URL
+
+# upload list of processes to be killed
+ps -e | grep avconv | cut -d ' ' -f1 | xargs -L1 echo kill | curl -F log_kill=@- -v $UPLOAD_URL
+
+# attempt to cleanup stale avconv processes
+ps -e | grep avconv | cut -d ' ' -f1 | xargs -L1 kill
 
 curl $SERVER_URL/ping
 update_hd_cam 192.168.1.10 restaurant
