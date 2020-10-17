4f0d88562a352175832529931ac31a99dc08fa46
jhpoelen <jhpoelen@xs4all.nl>
2020-10-17 13:51:48 -0700

    refactor log reporting

--- update_cam.sh
+++ update_cam.sh
@@ -21,14 +21,16 @@ update_whiteboard_lowres_cam() {
 	echo `date` update cam [$2] done.
 }
 
-# upload list of running pi processes
-ps -ef | curl -F log=@- -v $UPLOAD_URL
+upload_logs() {
+  # upload list of running pi processes
+  ps -ef | curl -F log=@- -v $UPLOAD_URL
 
-# upload list of processes to be killed
-ps -e | grep avconv | sed -E 's/^\s+//g' | curl -F log_kill=@- -v $UPLOAD_URL
+  # upload list of processes to be killed
+  ps -e | grep avconv | sed -E 's/^\s+//g' | curl -F log_kill=@- -v $UPLOAD_URL
 
-# attempt to cleanup stale avconv processes
-ps -e | grep avconv | sed -E 's/^\s+//g' | cut -d ' ' -f1 | xargs -L1 kill
+  # attempt to cleanup stale avconv processes
+  ps -e | grep avconv | sed -E 's/^\s+//g' | cut -d ' ' -f1 | xargs -L1 kill
+}
 
 curl $SERVER_URL/ping
 update_hd_cam 192.168.1.10 restaurant
