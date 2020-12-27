4ede9e166ccbd4f67d009e614fa143004ab123d5
jhpoelen <jhpoelen@xs4all.nl>
2020-12-26 19:49:53 -0800

    revert to profile 2

--- update_cam.sh
+++ update_cam.sh
@@ -12,7 +12,7 @@ update_cam() {
 update_trendnet_tv_ip110w_a() {
         # Trendnet TV-IP110W/A
 	echo `date` update cam [$2] starting...
-        curl -u admin:admin --globoff "http://$1/view.cgi?profile=1" | curl -F $2=@- -v $UPLOAD_URL
+        curl -u admin:admin --globoff "http://$1/view.cgi?profile=2" | curl -F $2=@- -v $UPLOAD_URL
 	echo `date` update cam [$2] done.
 }
 
